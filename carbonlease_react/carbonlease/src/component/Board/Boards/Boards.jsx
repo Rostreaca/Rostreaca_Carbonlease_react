3e062eeceb714e0a3864b4c21f12da47e6ddd6e2
yoonja486 <yoonja486@gmail.com>
2025-11-19 15:35:32 +0900

    board

--- carbonlease_react/carbonlease/src/component/Board/Boards/Boards.jsx
+++ carbonlease_react/carbonlease/src/component/Board/Boards/Boards.jsx
@@ -2,93 +2,41 @@ import PageTitle from '../../Common/Layout/PageTitle/PageTitle';
 import PageContent from '../../Common/PageContent/PageContent';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import Pagination from '../../Common/Pagination/Pagination'
-// useEffect를 사용하여 컴포넌트가 마운트될 때 데이터를 가져오고 상태를 업데이트합니다.
-// useState를 사용하여 상태 변수를 선언합니다.
+import Pagination from '../../Common/Pagination/Pagination';
 import BoardItems from './components/BoardItems';
 
+// useEffect를 사용하여 컴포넌트가 마운트될 때 데이터를 가져오고 상태를 업데이트합니다.
+// useState를 사용하여 상태 변수를 선언합니다.
+
  const Boards = () => {
-  
-    const [pageNumbers, setPageNumbers] = useState([]);
-    const [boardList, setBoardList] = useState([]);
-    const [totalPages, setTotalPages] = useState(1);
+
     const [currentPage, setCurrentPage] = useState(1);
+    const [board, setBoard] = useState([]);
+    const [pageInfo, setPageInfo] = useState({
+        startPage: 1,
+        endPage: 1,
+        totalPage: 1
+    });
 
-    useEffect(() => {
-        axios.get('http://localhost/boards?page=1')
-             .then(response => {
-                console.log(response.data);
-                console.log("전체 건수 : " + response.data.totalPages);
-                setTotalPages(response.data.totalPages);
-                setCurrentPage(response.data.currentPage);
-                setBoardList(response.data.boardList);
+    useEffect (()=>{
+        getBoards(currentPage);
+    }, [currentPage])
 
-            }            
-    )}, []);
-
-    const pageMove = (page) => {
-      axios.get(`http://localhost/boards?page=${page}`)
-             .then(response => {
-                console.log(response.data);
-                console.log("전체 건수 : " + response.data.totalPages);
-                setTotalPages(response.data.totalPages);
-                setCurrentPage(response.data.currentPage);
-                setBoardList(response.data.boardList);
-
-            } ) 
+    const getBoards = (page) => {
+        axios
+            .get(`http://localhost/boards?pageNo=${page}`)
+            .then((result) => {
+                console.log(result); // OK
+                const responseBoard = result.data.boards;
+                const responsePageInfo = result.data.pageInfo;
+                setBoard([...responseBoard]);
+                setPageInfo({
+                    startPage: responsePageInfo.startPage,
+                    endPage: responsePageInfo.endPage,
+                    totalPage: responsePageInfo.maxPage
+                })
+            })
     }
-     
-    const updatePageNumbers = (page, total) => {
-        const maxVisible = 5;
-        const blockNumber = Math.ceil(page / maxVisible);
-        const start = (blockNumber - 1) * maxVisible + 1;
-        const end = Math.min(blockNumber * maxVisible, total);
-        
-        const numbers = [];
-        for (let i = start; i <= end; i++) {
-            numbers.push(i);
-        }
-        setPageNumbers(numbers);
-    };
-
-    const handleFirstPage = () => {
-        console.log("handleFirstPage call !!");
-        setCurrentPage(1);
-        updatePageNumbers(1, totalPages);
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-
-    const handlePrevPage = () => {
-        if (currentPage > 1) {
-            const newPage = currentPage - 1;
-            setCurrentPage(newPage);
-            updatePageNumbers(newPage, totalPages);
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
-    };
-
-    const handlePageClick = (page) => {
-      console.log("페이지 클릭" , page) ;
-        setCurrentPage(page);
-        pageMove(page);
-        updatePageNumbers(page, totalPages);
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-
-    const handleNextPage = () => {
-        if (currentPage < totalPages) {
-            const newPage = currentPage + 1;
-            setCurrentPage(newPage);
-            updatePageNumbers(newPage, totalPages);
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
-    };
-
-    const handleLastPage = () => {
-        setCurrentPage(totalPages);
-        updatePageNumbers(totalPages, totalPages);
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
 
     return (
         <>
@@ -100,31 +48,17 @@ import BoardItems from './components/BoardItems';
                 ]} 
             />
             <PageContent>
-            {/* 전체 건수 : {totalPages} */}
-              {boardList.map((item) => (
-                // 각 리스트 아이템은 고유한 'key' prop을 가져야 합니다.
-                // <tr key={item.boardNo}>
-                //   <td>{item.boardSeq}</td>
-                //   <td>{item.boardTitle}</td>
-                //   <td>{item.nickname}</td>
-                //   <td>{item.enrollDate}</td>
-                //   <td>{item.viewCount}</td>
-                // </tr>
-                // 각 리스트 아이템은 고유한 'key' prop을 가져야 합니다.
-                <BoardItems key={item.boardNo} item={item}/>
-              ))}
-            
-          <Pagination
-                currentPage={currentPage}
-                totalPages={totalPages}
-                pageNumbers={pageNumbers}
-                onFirstPage={handleFirstPage}
-                onPrevPage={handlePrevPage}
-                onPageClick={handlePageClick}
-                onNextPage={handleNextPage}
-                onLastPage={handleLastPage}
-            />
+              
+                {
+                    board.map((item) => (
+                        <BoardItems key={item.boardNo} item={item} />
+                ))}
             </PageContent>
+           <Pagination
+            currentPage={currentPage} 
+            setCurrentPage={setCurrentPage}
+            pageInfo={pageInfo}
+            />
         </>
     )
 
--- carbonlease_react/carbonlease/src/component/Board/Boards/components/BoardItems.jsx
+++ carbonlease_react/carbonlease/src/component/Board/Boards/components/BoardItems.jsx
@@ -10,7 +10,7 @@ const BoardItems = ({ item }) => {
         <div className="title">{ item.boardTitle}</div>
         <div className="content">{ item.boardContent }</div>
         <div className="meta">
-          { item.enrollDate } · 조회 { item.viewCount } · 댓글 { }· 닉네임 { item.nickname }
+        작성일 : { item.enrollDate } · 조회 : { item.viewCount } · 댓글 : { item.replyCount } · 닉네임 : { item.nickname }
         </div>
       </BoardInfo>
     </BoardItemWrapper>
